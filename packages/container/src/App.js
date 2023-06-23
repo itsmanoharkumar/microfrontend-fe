85b25de66e1137b2aaef96baf22618e81042efde
Manohar <itsmanoharkumar@gmail.com>
2023-06-24 05:13:48 +0530

    Code committed

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import Header from "./components/Header";
 import {BrowserRouter} from "react-router-dom";
+import {createGenerateClassName, StylesProvider} from "@material-ui/core";
 
 export default () => {
     return (
+        <StylesProvider generateClassName={createGenerateClassName({
+            productionPrefix: 'co'
+        })}>
         <BrowserRouter>
             <div>
                 <Header/>
                 <MarketingApp/>
             </div>
         </BrowserRouter>
+        </StylesProvider>
     );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -4,10 +4,13 @@ import {StylesProvider} from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
+import {createGenerateClassName} from "@material-ui/core";
 
 export default () => {
     return (
-        <StylesProvider>
+        <StylesProvider generateClassName={createGenerateClassName({
+            productionPrefix: 'ma'
+        })}>
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/pricing" component={Pricing}/>
